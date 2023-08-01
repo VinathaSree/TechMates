a059b73caf71d8cccebaa041997bcd1fe0dcb277
Vinatha_Sree <vinatha.111@gmail.com>
2023-08-01 18:25:12 +0530

    Add styling for mainContent file.

--- components/header.jsx
+++ components/header.jsx
@@ -17,10 +17,11 @@ const Header = () => {
       <span class="ml-3 text-xl">Techmates</span>
     </a>
     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+    <a class="mr-5 hover:text-gray-900">Home</a>
       <a class="mr-5 hover:text-gray-900">About</a>
-      <a class="mr-5 hover:text-gray-900">Hackathon</a>
+      <a class="mr-5 hover:text-gray-900">Communication</a>
+      <a class="mr-5 hover:text-gray-900">Hackathons</a>
       <a class="mr-5 hover:text-gray-900">Peers</a>
-
     </nav>
     <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Profile
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
